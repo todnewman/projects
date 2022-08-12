2be63c306aaa9e49c54ca8a6de5a285587925212
Tod Newman <todnewman@gmail.com>
2022-08-12 14:25:47 -0700

    Add files via upload

--- weather/Plot_Lightning_Data.py
+++ weather/Plot_Lightning_Data.py
@@ -1,41 +1,34 @@
-
+#!/home/tod/anaconda3/bin/python3
 # coding: utf-8
 
 # In[11]:
 
 
 from __future__ import division
+PATH="/home/tod/anaconda3/bin"
 import matplotlib
 matplotlib.use('Agg')
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 import datetime
+from datetime import date
 import sys
 import scipy
 
 sys.stdout = open("out.txt","w")
 sys.stderr = open("err.txt","w")
-#get_ipython().run_line_magic('matplotlib', 'inline')
-
 
 datafile = "/home/tod/data/lightning_sensor/lightning_sensor_data.csv"
-output_file = "/home/tod/data/lightning_sensor/lightning_sensor_data_plot.pdf"
+
 title = 'Tucson Lightning Sensor Data'
 
-# Values to normalize environmental data
+# Values to normalize environmental data - done primarily to make the plot look better.
 norm_pressure = 900
 
-
 #Read .CSV file into a Pandas Data Structure below
-
-#df = pd.read_csv(datafile,  infer_datetime_format='True',  date_parser=parse_dates, dayfirst=True, low_memory=False)
 df = pd.read_csv(datafile, parse_dates=True, infer_datetime_format='True', dayfirst=True, low_memory=False)
-#df = pd.read_csv(datafile, index_col='Time', parse_dates=True, infer_datetime_format='True', dayfirst=True, low_memory=False)
-#df['Time'] = pd.to_datetime(df['Time'].str.strip(), dayfirst = False)
 df = df.dropna()
-
-
 
 #Fix Date Formats Below
 
@@ -43,11 +36,11 @@ def parse_dates(x):
     return datetime.datetime.strptime(x, '%H:%M:%S - %Y/%m/%d')
     
 x = []
-
+dist_filter = df['Distance'] > 0
 timestamp = df['Time']
 y1 = df['Humidity (%)']
 y1b = df['Distance']
-y1c = df['Pressure (HP-900)'] - norm_pressure
+y1c = (df['Pressure (HP-900)'] - norm_pressure)*3
 y1d = df['Temperature (deg F)']
 y1e = df['Full Light Spectrum'] / 5.0
 y1f = df['IR Spectrum'] / 5.0
@@ -56,52 +49,77 @@ y2 = df['Energy'] / 3.0
 for t in timestamp:
     x.append(parse_dates(t))
   
-d = scipy.zeros(len(y1f))
+
+today = date.today()
+now = datetime.datetime.now()
+
+def plot_data(period, date_lim):  
+    output_file = ("/home/tod/data/lightning_sensor/lightning_sensor_data_plot-%s.pdf" % period)    
+    d = scipy.zeros(len(y1f))
+    with PdfPages(output_file) as pdf:
+        try:
+            fig, ax1 = plt.subplots(figsize=(12, 8))
+            plt.xticks(rotation=45)
+            ax2 = ax1.twinx()
+            ax1.plot(x, y1, 'g-')
+            ax1.plot(x, y1c, 'c-')
+            ax1.plot(x, y1d, 'm-')
+            ax1.plot(x, y1e, 'b:')
+            ax1.fill_between(x,y1e,where=y1f>=d, interpolate=True, color='lightcyan')  
+            ax1.plot(x, y1f, 'g:')
+            ax1.fill_between(x,y1f,where=y1f>=d, interpolate=True, color='greenyellow')
+            ax2.plot(x, y2, 'b-')
+            ax1.plot(x, y1b, 'r*')
+
+            ax1.set_xlabel('Time/Date')
+            ax1.set_ylabel('Environmental Data and Lightning Distance', color='k')
+            #ax1.set_xlim(left=datetime.date(2018, 7, 20))
+            ax1.set_ylim(0,120)
+            #ax1.set_xlim(left=date_lim, right=today)
+            ax1.set_xlim(left=date_lim, right=now)
+            ax2.set_ylabel('Lightning Energy', color='b')
+            ax2.set_ylim(0,500000)
+    
+            #ax2.legend(loc='best')
+            ax1.legend(loc='best')
+            plt.xticks(rotation=45)
+
+            plt.title('Lightning activity fused with Weather data for Tucson (85718)')
+            #plt.show()
+    
+            fig.savefig('/home/tod/data/lightning_sensor/lightning_%s.png' % period)
+
+            pdf.savefig(fig)  # or you can pass a Figure object to pdf.savefig
+            plt.close()
+
+            # We can also set the file's metadata via the PdfPages object:
+            d = pdf.infodict()
+            d['Title'] = title
+            d['Author'] = u'W. Tod Newman'
+            d['Subject'] = 'Fusion of Lightning sensor data with environmental sensor data'
+            d['Keywords'] = 'lightning, tucson, humidity, pressure, temperature'
+            d['CreationDate'] = datetime.datetime(2018, 7, 13)
+            d['ModDate'] = datetime.datetime.today()
+        except Exception as error:
+            print( "<p>Error: %s</p>" % str(error) )
+
+for period in ['day', 'week', 'month', 'year']:
+    if period == 'day':
+        #date_lim = date(today.year, today.month, today.day-1)
+        date_lim = now - datetime.timedelta(days=2)
+        plot_data(period, date_lim)
+    if period == 'week':
+        date_lim = now - datetime.timedelta(days=7)
+        plot_data(period, date_lim)
+    if period == 'month':
+        date_lim = now - datetime.timedelta(days=20)
+        plot_data(period, date_lim)
+    if period == 'year':
+        try:
+            date_lim = today - datetime.timedelta(days=60)
+            plot_data(period, date_lim)
+        except:
+            print("We must not have a full year's worth of data yet")
+            print(period, date_lim)
   
-with PdfPages(output_file) as pdf:
-    try:
-        fig, ax1 = plt.subplots(figsize=(12, 8))
-        plt.xticks(rotation=45)
-        ax2 = ax1.twinx()
-        ax1.plot(x, y1, 'g-')
-        ax1.plot(x, y1b, 'r*')
-        ax1.plot(x, y1c, 'c-')
-        ax1.plot(x, y1d, 'm-')
-        ax1.plot(x, y1e, 'b:')
-        ax1.fill_between(x,y1e,where=y1f>=d, interpolate=True, color='lightcyan')  
-        ax1.plot(x, y1f, 'g:')
-        ax1.fill_between(x,y1f,where=y1f>=d, interpolate=True, color='greenyellow')  
-        ax2.plot(x, y2, 'b-')
-
-        ax1.set_xlabel('Time/Date')
-        ax1.set_ylabel('Environmental Data and Lightning Distance', color='k')
-        #ax1.set_xlim(left=datetime.date(2018, 7, 20))
-        ax2.set_ylabel('Lightning Energy', color='b')
-    
-        #ax2.legend(loc='best')
-        ax1.legend(loc='best')
-    
-        plt.xticks(rotation=45)
-
-        plt.title('Lightning activity fused with Weather data for Tucson (85718)')
-        #plt.show()
-    
-        fig.savefig('/home/tod/data/lightning_sensor/lightning1.png')
-
-
-
-        pdf.savefig(fig)  # or you can pass a Figure object to pdf.savefig
-        plt.close()
-
-        # We can also set the file's metadata via the PdfPages object:
-        d = pdf.infodict()
-        d['Title'] = title
-        d['Author'] = u'W. Tod Newman\xe4nen'
-        d['Subject'] = 'Fusion of Lightning sensor data with environmental sensor data'
-        d['Keywords'] = 'lightning, tucson, humidity, pressure, temperature'
-        d['CreationDate'] = datetime.datetime(2018, 7, 13)
-        d['ModDate'] = datetime.datetime.today()
-    except Exception as error:
-        print( "<p>Error: %s</p>" % str(error) )
-
         
--- weather/lightning_sensor.py
+++ weather/lightning_sensor.py
@@ -1,17 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-'''
-This code integrates a Raspberry Pi with a number of environmental sensors:
-* The AS3935 Lightning Sensor
-* The BME280 Environmental Sensor
-* The TSL2591 Luminosity Sensor
-
-This data is all fused on the device, pulled into a DataFrame, and output
-as a .csv
-
-Other modules not optimized for the RPi then generate this data into plots.
-'''
-
 from RPi_AS3935 import RPi_AS3935
 import RPi.GPIO as GPIO
 import thread
@@ -23,65 +11,48 @@ from Adafruit_BME280 import *
 from threading import Thread
 
 
-filename = '/home/pi/lightning_sensor_data.csv'  #output file name
+filename = '/home/pi/lightning_sensor_data.csv'
 
 test = False
 
-#
 # Initialize GPIO
-#
 GPIO.setmode(GPIO.BCM)
 print ("GPIO setmode")
 
 # pin used for interrupts
 pin = 17
-#
-# Rev 1 Raspberry Pis should leave bus set at 0, while rev. 2 Pis should set
+# 1 Raspberry Pis should leave bus set at 0, while rev. 2 Pis should set
 # bus equal to 1. The address should be changed to match the address of the
 # sensor.
-#
-sensor = RPi_AS3935(address=0x03, bus=1)
-sensor.reset()
 
-#
-# BME280 Environmental Sensor: Humidity, Temperature, Pressure
-#
+#sensor = RPi_AS3935(address=0x03, bus=1)
+#sensor.reset()
 sensor_env = BME280(p_mode=BME280_OSAMPLE_8, t_mode=BME280_OSAMPLE_2, h_mode=BME280_OSAMPLE_1, filter=BME280_FILTER_16)
 
-#
-# Change this value to the tuning value for your sensor.  It's probably going
-# to be on the antistatic bag or other documentation from the supplier.
-#
-time.sleep(0.005) # delay to let devices stabilize
-sensor.calibrate(tun_cap=0x02)
+# Change this value to the tuning value for your sensor
+
+time.sleep(0.005)
+#sensor.calibrate(tun_cap=0x02)
 time.sleep(0.005)
 
-#
-# Indoors = more sensitive (can miss very strong lightning events)
-# Outdoors = less sensitive (can miss far away lightning events)
-#
-sensor.set_indoors(False)
-sensor.set_noise_floor(1)
+# Indoors = more sensitive (can miss very strong lightnings)
+# Outdoors = less sensitive (can miss far away lightnings)
 
-#
-# Prevent single isolated strikes from being logged 
-#     => interrupts begin after 5 strikes, then are fired normally
-#
-sensor.set_min_strikes(1)
+#sensor.set_indoors(False)
+#sensor.set_noise_floor(1)
+
+
+# Prevent single isolated strikes from being logged => interrupts begin after 5 strikes, then are fired normally
+
+#sensor.set_min_strikes(1)
 
 last_alert = datetime.min
 strikes_since_last_alert = 0
 
-#
-# These are the headers for our fused DataFrame
-#
 cols = ['Time', 'Energy', 'Distance', 'Temperature (deg F)', 'Pressure (HP-900)', 'Humidity (%)','Full Light Spectrum', 'IR Spectrum', 'Luminosity (lux)']
 
 def data_collect(cur_time, energy, distance):
-    #
-    #  Data collection routine for the environmental sensor.  We pass in data
-    #  from the lightning sensor so we can fuse more easily in this function.
-    #
+ 
     lst = []
     degrees = sensor_env.read_temperature()*9.0/5.0+32
     pascals = sensor_env.read_pressure()
@@ -111,10 +82,6 @@ def periodic_data():
     data_collect(cur_time, energy, distance)
 
 def call_tsl2591():
-    #
-    # Driver for the Luminosity sensor.  This sensor gets visual and near-IR
-    # Luminosity.
-    #
     import tsl2591
 
     tsl = tsl2591.Tsl2591()  # initialize
@@ -125,25 +92,20 @@ def call_tsl2591():
 
 # Interrupt handler
 def handle_interrupt( channel):
-    #
-    # Interrupt service routine for the Lightning Sensor.
-    #
     global last_alert
     global strikes_since_last_alert
     global sensor
     global LastStatus
     global LastInterrupt
-    
-    max_noise_val = 7
     current_timestamp = datetime.now()
-    time.sleep(0.003) # delay to let the system stabilize
+    time.sleep(0.003)
     
-    #
     # Set up Pandas Dataframe for data collection
-    #
+    
     logf = open("/home/pi/error.log", "a")
     try:
-        reason = sensor.get_interrupt()
+        #reason = sensor.get_interrupt()
+        reason = 15
         LastInterrupt = reason
     except IOError:
         subprocess.call(['i2cdetect', '-y', '1'])
@@ -153,10 +115,12 @@ def handle_interrupt( channel):
     elif reason == 0x01:
         LastStatus = 'Noise level too high'
         noise = sensor.get_noise_floor()
-        if (noise < max_nose_val): 
+        if (noise < 7):
             print("Noise level too high - adjusting")
             logf.write("%s: External Noise level is too high - adjusting noise floor to be %s\n" % (current_timestamp, noise+1))
             sensor.raise_noise_floor()
+            #noise = sensor.get_noise_floor()
+            #logf.write("Noise level now at %s\n" % noise)
     elif reason == 0x04:
         LastStatus = 'Disturber detected'
         print("Disturber detected. Masking subsequent disturbers")
@@ -177,11 +141,7 @@ def handle_interrupt( channel):
         print("Energy: " + str(energy) + " - distance: " + str(distance) + "km")
         last_alert = current_timestamp
         data_collect(cur_time, energy, distance)
-        
-    #
-    # If no strike has been detected for the last hour, reset the 
-    # strikes_since_last_alert (consider storm finished)
-    #    
+    # If no strike has been detected for the last hour, reset the strikes_since_last_alert (consider storm finished)
     elif test:
         cur_time = current_timestamp.strftime('%H:%M:%S - %Y/%m/%d')
         distance = 40
@@ -212,18 +172,15 @@ def wait_for_lightning():
         GPIO.cleanup()
         
 if __name__ == '__main__':
-    #
-    # Setu up to receive interrupts.
+
     # Use a software Pull-Down on interrupt pin
-    #
     GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    sensor.set_mask_disturber(False)
+    #GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    #sensor.set_mask_disturber(False)
     GPIO.add_event_detect(pin, GPIO.RISING, callback=handle_interrupt, bouncetime=300)
-    
-    #
-    #  Set up a Lightning thread and a Environmental Sensor Thread
-    #
-    Thread(target=wait_for_lightning).start() # Start watching for lightning.  We'll always trigger any time it comes in.
+    #GPIO.add_event_detect(pin, GPIO.FALLING, callback=handle_interrupt, bouncetime=300)
+
+    #Thread(target=wait_for_lightning).start() # Start watching for lightning.  We'll always trigger any time it comes in.
     Thread(target=periodic_data).start() # Look for environmental data every hour. Initialize here. 
     while True:
         sleep(3600)
